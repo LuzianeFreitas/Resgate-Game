c749733f10bdab155e65abc414357cce85ca103f
Luziane Freitas <freitas.lu@outlook.com>
2021-06-15 21:47:29 -0300

    add: Game over

--- js/js.js
+++ js/js.js
@@ -29,15 +29,15 @@ function start() { // Inicio da função start()
 
 	jogo.pressionou = [];
 
-	var somDisparo=document.getElementById("somDisparo");
-	var somExplosao=document.getElementById("somExplosao");
-	var musica=document.getElementById("musica");
-	var somGameover=document.getElementById("somGameover");
-	var somPerdido=document.getElementById("somPerdido");
-	var somResgate=document.getElementById("somResgate");
+	var somDisparo = document.getElementById("somDisparo");
+	var somExplosao = document.getElementById("somExplosao");
+	var musica = document.getElementById("musica");
+	var somGameover = document.getElementById("somGameover");
+	var somPerdido = document.getElementById("somPerdido");
+	var somResgate = document.getElementById("somResgate");
 
 	//Música em loop
-	musica.addEventListener("ended", function(){ musica.currentTime = 0; musica.play(); }, false);
+	musica.addEventListener("ended", function () { musica.currentTime = 0; musica.play(); }, false);
 	musica.play();
 
 	var velocidade = 5;
@@ -194,7 +194,7 @@ function start() { // Inicio da função start()
 
 		if (colisao1.length > 0) {
 			energiaAtual--;
-			velocidade=velocidade+0.3;
+			velocidade = velocidade + 0.3;
 			inimigo1X = parseInt($("#inimigo1").css("left"));
 			inimigo1Y = parseInt($("#inimigo1").css("top"));
 			explosao1(inimigo1X, inimigo1Y);
@@ -411,10 +411,39 @@ function start() { // Inicio da função start()
 
 			$("#energia").css("background-image", "url(imgs/energia0.png)");
 
-			//Game Over
+			gameOver();
 		}
 
 	} // Fim da função energia()
 
+	//Função GAME OVER
+	function gameOver() {
+		fimdejogo = true;
+		musica.pause();
+		somGameover.play();
+
+		window.clearInterval(jogo.timer);
+		jogo.timer = null;
+
+		$("#jogador").remove();
+		$("#inimigo1").remove();
+		$("#inimigo2").remove();
+		$("#amigo").remove();
+
+		$("#fundoGame").append("<div id='fim'></div>");
+
+		$("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
+	} // Fim da função gameOver();
+
 } // Fim da função start
+
+
+//Reinicia o Jogo
+		
+function reiniciaJogo() {
+	somGameover.pause();
+	$("#fim").remove();
+	start();
+	
+} //Fim da função reiniciaJogo
 
